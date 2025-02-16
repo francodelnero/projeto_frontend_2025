f078e39fe98ffd6bc4c467f6a99ad2b20227765b
@francodelnero <francodelnero@hotmail.com>
2025-02-16 19:03:17 -0300

    update

---
+++ semana1.js
@@ -0,0 +1,15 @@
+/*  
+tudo isso daqui
+serão comentários
+de várias linhas até chegar
+no próximo asterísco 
+*/
+
+//ou isso daqui também, mas somente uma linha
+
+let nome = "Gustavo";
+const idade = 3;
+console.log("Olá, mundo!");
+alert("Testando o Alert!");
+
+
